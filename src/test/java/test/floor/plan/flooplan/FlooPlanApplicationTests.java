cf29ef779e1f822918ab673c0c287460d29dbb84
Andrewmazyar <maziarandriy@gmail.com>
2020-07-09 14:31:45 +0300

    added one more test

--- src/test/java/test/floor/plan/flooplan/FlooPlanApplicationTests.java
+++ src/test/java/test/floor/plan/flooplan/FlooPlanApplicationTests.java
@@ -7,10 +7,11 @@ import org.junit.Before;
 import org.junit.Test;
 import test.floor.plan.flooplan.models.Points;
 import test.floor.plan.flooplan.util.FloorPlan;
+import test.floor.plan.flooplan.util.ParseData;
 
 public class FlooPlanApplicationTests {
     private FloorPlan floorPlan;
-    private Points points;
+    private ParseData parseData;
     List<Points> pointsList;
     List<Points> pointsList1;
     List<Points> wrongList;
@@ -19,57 +20,57 @@ public class FlooPlanApplicationTests {
     @Before
     public void setUp() {
         floorPlan = new FloorPlan();
-        points = new Points();
-        pointsList = new ArrayList<>();
         pointsList1 = new ArrayList<>();
         wrongList = new ArrayList<>();
         wrongList1 = new ArrayList<>();
+        parseData = new ParseData();
 
+        Points points = new Points();
         points.setCoordinateX(1);
         points.setCoordinateY(1);
-        pointsList.add(points);
         pointsList1.add(points);
         wrongList.add(points);
 
-        points.setCoordinateX(1);
-        points.setCoordinateY(2);
-        pointsList.add(points);
-        pointsList1.add(points);
-        wrongList.add(points);
+        Points points1 = new Points();
+        points1.setCoordinateX(1);
+        points1.setCoordinateY(2);
+        pointsList1.add(points1);
+        wrongList.add(points1);
 
-        points.setCoordinateX(2);
-        points.setCoordinateY(2);
-        pointsList.add(points);
+        Points points2 = new Points();
+        points2.setCoordinateX(0);
+        points2.setCoordinateY(2);
+        pointsList1.add(points2);
 
-        points.setCoordinateX(2);
-        points.setCoordinateY(1);
-        pointsList.add(points);
+        Points points3 = new Points();
+        points3.setCoordinateX(0);
+        points3.setCoordinateY(3);
+        pointsList1.add(points3);
 
-        points.setCoordinateX(0);
-        points.setCoordinateY(2);
-        pointsList1.add(points);
+        Points points4 = new Points();
+        points4.setCoordinateX(2);
+        points4.setCoordinateY(3);
+        pointsList1.add(points4);
 
-        points.setCoordinateX(0);
-        points.setCoordinateY(3);
-        pointsList1.add(points);
-
-        points.setCoordinateX(2);
-        points.setCoordinateY(3);
-        pointsList1.add(points);
-
-        points.setCoordinateX(2);
-        points.setCoordinateY(1);
-        pointsList1.add(points);
+        Points points5 = new Points();
+        points5.setCoordinateX(2);
+        points5.setCoordinateY(1);
+        pointsList1.add(points5);
     }
 
     @Test
     public void testOk() {
-        Assert.assertEquals(true, floorPlan.floorChecker(pointsList));
         Assert.assertEquals(true, floorPlan.floorChecker(pointsList1));
     }
+
     @Test
     public void testWrong() {
         Assert.assertEquals(false, floorPlan.floorChecker(wrongList));
         Assert.assertEquals(false, floorPlan.floorChecker(wrongList1));
+    }
+
+    @Test
+    public void testParseOk() {
+        Assert.assertEquals(pointsList1, parseData.parse("1,1;1,2;0,2;0,3;2,3;2,1"));
     }
 }
